ad0da27ef43957af8be04767197a50ff75d5ca09
gabhiinav <gabhiinav@gmail.com>
2023-07-06 18:27:19 +0530

    Types

---
+++ types/types.ts
@@ -0,0 +1,11 @@
+export type network = {
+  name: string;
+  img?: string | null;
+  logo_path: string | null;
+};
+
+export type suggestion = {
+  id: number;
+  name: string;
+  poster_path: string;
+};
