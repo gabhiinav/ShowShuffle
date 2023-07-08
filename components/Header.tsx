722df22ef2f65af923d003a2c3d339cf17b41570
gabhiinav <gabhiinav@gmail.com>
2023-07-08 16:55:25 +0530

    Header

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Logo from "../Logo";
+
+const Header = () => {
+  return (
+    <header className="w-full absolute top-0 flex justify-center py-8 bg-fade-effect">
+      <Link href="/" passHref>
+        <a className="transition-all hover:opacity-50">
+          <Logo />
+        </a>
+      </Link>
+    </header>
+  );
+};
+
+export default Header;
