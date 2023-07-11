ea8de3c91fb86d6da7d6198f0667e5763184d07c
gabhiinav <gabhiinav@gmail.com>
2023-07-11 18:18:34 +0530

    Design

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <div className="flex items-center gap-2 w-full">
-      <Image src="logo.svg" alt="Logo" width={32} height={32} />
+      <Image src="logo.svg" alt="Logo" width={40} height={40} />
       <span className="text-2xl sm:text-4xl font-bold text-brand-400">
         ShowShuffle
       </span>
