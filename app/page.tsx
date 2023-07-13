731f4a3dc3c947962f90db8f562ad67bb7752a62
gabhiinav <gabhiinav@gmail.com>
2023-07-14 02:01:23 +0530

    Remove Head

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Head from "next/head";
 import Logo from "@/components/Logo";
 import SearchInput from "@/components/SearchInput";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>ShowShuffle | Enjoy without knowing how!</title>
-      </Head>
       <div className="flex flex-col lg:flex-row items-center justify-center gap-8 min-h-screen p-4">
         <aside className="flex items-left flex-col gap-3 max-w-sm">
           <Logo />
